c3038da96fee7ac4c1b1c04c94936ed4804e8e29
Aaron Helton <marisagricola@gmail.com>
2015-10-27 15:12:02 +0000

    initial commit

---
+++ votedb/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from votedb import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^turtle/(?P<uri>http\:\/\/.+)$', views.turtle, name='turtle'),
+    url(r'^session/(?P<uri>http\:\/\/.+)$', views.session_record, name='sesion_record'),
+    url(r'^memberstate/(?P<uri>http\:\/\/.+)$', views.member_state, name='member_state'),
+    url(r'^bymember/(?P<uri>http\:\/\/.+)$', views.by_member, name='by_member'),
+    url(r'^voterecord/(?P<uri>http\:\/\/.+)$', views.vote_record, name='vote_record'),
+    url(r'^memberstates/$', views.member_states, name='member_states'),
+]
